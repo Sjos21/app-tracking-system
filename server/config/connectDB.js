c92837aa0666f20c15b8ab8920369f1c2c5b4380
shivu joshi <shivranjanjoshi@gmail.com>
2025-06-11 00:54:45 +0530

    new changes

---
+++ server/config/connectDB.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectDB = () => {
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("Database connected"))
+    .catch((error) => {
+      console.log("Error connecting database");
+      console.error(error.message);
+      process.exit(1);
+    });
+}
+export default connectDB;
